c7bdc36240378f116635d50d2b3903d94614cd20
Maciej Borkowski <33153765+macborkow@users.noreply.github.com>
2020-10-22 13:27:37 +0200

    Add init function

--- assets/js/calculator.js
+++ assets/js/calculator.js
@@ -1,1 +1,15 @@
+let digitsSpace = document.getElementById("digits");
 
+
+function init() {
+	let digits = [];
+	for(let i = 0; i < 9; ++i) {
+		digits.push(document.createElement("div"));
+		digits[i].classList.add("button");
+		digits[i].id = i.toString();
+		digits[i].appendChild(document.createTextNode(i.toString()));
+		digitsSpace.appendChild(digits[i]);
+	}
+}
+
+document.onload = init();
