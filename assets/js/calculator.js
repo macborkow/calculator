431156988d9129d86560d74889afb7a67b153c7b
Maciej Borkowski <33153765+macborkow@users.noreply.github.com>
2020-10-23 15:23:01 +0200

    Fix buttons positioning

--- assets/js/calculator.js
+++ assets/js/calculator.js
@@ -3,12 +3,12 @@ let digitsSpace = document.getElementById("digits");
 
 function init() {
 	let digits = [];
-	for(let i = 0; i < 9; ++i) {
+	for(let i = 1; i <= 9; ++i) {
 		digits.push(document.createElement("div"));
-		digits[i].classList.add("button");
-		digits[i].id = i.toString();
-		digits[i].appendChild(document.createTextNode(i.toString()));
-		digitsSpace.appendChild(digits[i]);
+		digits[i-1].classList.add("button");
+		digits[i-1].id = i.toString();
+		digits[i-1].appendChild(document.createTextNode(i.toString()));
+		digitsSpace.appendChild(digits[i-1]);
 	}
 }
 
