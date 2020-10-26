95481c6e87342c1ad6ccb9aebac8c783ba40f129
Maciej Borkowski <33153765+macborkow@users.noreply.github.com>
2020-10-26 11:38:15 +0100

    add the operation buttons

--- assets/js/calculator.js
+++ assets/js/calculator.js
@@ -1,8 +1,10 @@
 let digitsSpace = document.getElementById("digits");
-
+let operationsSpace = document.getElementById("operations");
+let screenSpace = document.getElementById("screen");
+let digits = [];
+let operations = {};
 
 function init() {
-	let digits = [];
 	for(let i = 1; i <= 9; ++i) {
 		digits.push(document.createElement("div"));
 		digits[i-1].classList.add("button");
@@ -10,6 +12,48 @@ function init() {
 		digits[i-1].appendChild(document.createTextNode(i.toString()));
 		digitsSpace.appendChild(digits[i-1]);
 	}
+	screenSpace.appendChild(document.createTextNode("HI WORLD ."));
+	operations["C"] = document.createElement("div");
+	operations["C"].classList.add("button");
+	operations["C"].classList.add("opbutton");
+	operations["C"].id = "clear";
+	operations["C"].appendChild(document.createTextNode("C"));
+	
+	operationsSpace.appendChild(operations["C"]);
+	operations["+"] = document.createElement("div");
+	operations["+"].classList.add("button");
+	operations["+"].classList.add("opbutton");
+	operations["+"].id = "plus";
+	operations["+"].appendChild(document.createTextNode("+"));
+	operationsSpace.appendChild(operations["+"]);
+		
+	operations["-"] = document.createElement("div");
+	operations["-"].classList.add("button");
+	operations["-"].classList.add("opbutton");
+	operations["-"].id = "minus";
+	operations["-"].appendChild(document.createTextNode("-"));
+	operationsSpace.appendChild(operations["-"]);
+	
+	operations["."] = document.createElement("div");
+	operations["."].classList.add("button");
+	operations["."].classList.add("opbutton");
+	operations["."].id = "dot";
+	operations["."].appendChild(document.createTextNode("."));
+	operationsSpace.appendChild(operations["."]);
+	
+	operations["*"] = document.createElement("div");
+	operations["*"].classList.add("button");
+	operations["*"].classList.add("opbutton");
+	operations["*"].id = "multiply";
+	operations["*"].appendChild(document.createTextNode("*"));
+	operationsSpace.appendChild(operations["*"]);
+	
+	operations["/"] = document.createElement("div");
+	operations["/"].classList.add("button");
+	operations["/"].classList.add("opbutton");
+	operations["/"].id = "divide";
+	operations["/"].appendChild(document.createTextNode("÷"));
+	operationsSpace.appendChild(operations["/"]);
 }
 
 document.onload = init();
